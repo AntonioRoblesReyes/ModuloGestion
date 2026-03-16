2499edf14f69dcc87a57acba4ea28428f826432c
Antonio Robles <antoniorobles63@hotmail.com>
2026-03-16 19:21:12 -0400

    Cambios login next

--- ModuloGestion.Next/ModuloGestion.Next.UI.WinForms/Forms/Auth/FrmLoginNext.vb
+++ ModuloGestion.Next/ModuloGestion.Next.UI.WinForms/Forms/Auth/FrmLoginNext.vb
@@ -47,7 +47,9 @@ Namespace Forms.Auth
             Me.Close()
         End Sub
 
+        Private Sub FrmLoginNext_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
+        End Sub
     End Class
 
 End Namespace
