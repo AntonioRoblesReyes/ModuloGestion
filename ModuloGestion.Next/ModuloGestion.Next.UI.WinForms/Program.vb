57cde4f8fe22be57ea0334212ac019f48bec1db1
AntonioRoblesReyes <antoniorobles63@hotmail.com>
2026-03-16 17:55:16 -0400

    Align startup/login flow with FrmLoginNext partial form

--- ModuloGestion.Next/ModuloGestion.Next.UI.WinForms/Program.vb
+++ ModuloGestion.Next/ModuloGestion.Next.UI.WinForms/Program.vb
@@ -1,12 +1,19 @@
 Imports System
-Imports System.Windows.Forms
 
 Namespace Bootstrap
     Friend Module Program
         <STAThread>
         Public Sub Main()
-            ApplicationConfiguration.Initialize()
-            Application.Run(New Forms.Common.FrmShell())
+            Global.System.Windows.Forms.Application.EnableVisualStyles()
+            Global.System.Windows.Forms.Application.SetCompatibleTextRenderingDefault(False)
+
+            Using frmLogin As New Forms.Auth.FrmLoginNext()
+                If frmLogin.ShowDialog() <> Global.System.Windows.Forms.DialogResult.OK Then
+                    Return
+                End If
+            End Using
+
+            Global.System.Windows.Forms.Application.Run(New Forms.Common.FrmShell())
         End Sub
     End Module
 End Namespace
